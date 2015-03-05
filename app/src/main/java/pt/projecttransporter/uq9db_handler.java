0b46d7374247b21dc17304d22a715f3503c8bd2f
AurionRaid <arionarisworoputro@yahoo.com>
2015-03-05 14:27:36 +1000

    Update uq9db_handler.java

--- app/src/main/java/pt/projecttransporter/uq9db_handler.java
+++ app/src/main/java/pt/projecttransporter/uq9db_handler.java
@@ -20,7 +20,7 @@ public class uq9db_handler extends SQLiteOpenHelper{
   // All static variables
   // Ensuring only and ONLY one instance of DatabaseHelper is 
 	// existing at any given time
-	private static uq9db_handler db_instance;
+	private static uq9db_handler uq9db_instance;
 	
 	// Database version
 	// Database Name and Paths
@@ -32,10 +32,80 @@ public class uq9db_handler extends SQLiteOpenHelper{
 
 	// Contacts Table Columns names
 	private static final String K_ID = "id";
-	private static final String K_NAME = "name";
+	private static final String K_FIRSTNAME = "firstname";
+	private static final String K_LASTNAME = "lastname";
 	private static final String K_GENDER = "gender";
-	private static final String K_DRIVER = "driver";
-	private static final String K_STUDENT = "student";
-	private static final String K_CARCAP = "carcap";
-	private static final String K_GOING = "going";
+	private static final String K_ADDR = "address";
+	private static final String K_TEL = "tel";
+	private static final String K_DOB = "dob";
+	private static final String K_EMAIL = "email";
+	private static final String K_FBLINK = "fblink";
+	private static final String K_NATIONALITY = "nationality";
+	private static final String K_WORKSTAT = "work_stat";
+	private static final String K_DRIVE = "drive?";
+	private static final String K_CARSPACE = "carspace";
+	private static final String K_GOCARD = "gocard";
+	
+	// Use the application context, which will ensure that you 
+	// don't accidentally leak an Activity's context.
+	// See this article for more information: http://bit.ly/6LRzfx
+	public static uq9db_handler getInstance(Context context) {
+		if (uq9db_instance == null) {
+			uq9db_instance = new uq9db_handler(context.getApplicationContext());
+		}
+		return uq9db_instance;
+	}
+	
+	/* Constructor should be private to prevent direct instantiation.
+	make call to static factory method "getInstance()" instead */
+	private DatabaseHandler(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	// Creating Tables
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// Check whether database is already exist on path, to ensure 
+		// one time call for creating database so that it does not overwrite the old
+		// one everytime the application starts
+		if (!uq9db_instance.isDatabaseExist()){
+
+			// Create table - Main
+			String create_main_table = "CREATE TABLE " + TABLE_CONTACTS + "("
+					+ K_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+					+ KEY_GENDER + " TEXT," + KEY_DRIVER + " TEXT," + KEY_STUDENT  + " TEXT," 
+					+ KEY_CARCAP + " TEXT," + KEY_GOING + " TEXT " + ")";
+
+			// Execute SQL command
+			db.execSQL(create_main_table);
+			
+			// Create table - Drivers
+			String create_drivers_table = "CREATE TABLE " + TABLE_CONTACTS + "("
+					+ KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+					+ KEY_GENDER + " TEXT," + KEY_DRIVER + " TEXT," + KEY_STUDENT  + " TEXT," 
+					+ KEY_CARCAP + " TEXT," + KEY_GOING + " TEXT " + ")";
+
+			// Execute SQL command
+			db.execSQL(create_drivers_table);
+			
+			// Create table - GoCard
+			String create_gocard_table = "CREATE TABLE " + TABLE_CONTACTS + "("
+					+ KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+					+ KEY_GENDER + " TEXT," + KEY_DRIVER + " TEXT," + KEY_STUDENT  + " TEXT," 
+					+ KEY_CARCAP + " TEXT," + KEY_GOING + " TEXT " + ")";
+
+			// Execute SQL command
+			db.execSQL(create_gocard_table);
+		}
+	}
+
+	// Upgrading database
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Drop older table if existed
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+
+		// Create tables again
+		onCreate(db);
+	}
 }
