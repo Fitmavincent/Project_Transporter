5d79f108968b32b8b3687068d976f36dfb30e31d
AurionRaid <arionarisworoputro@yahoo.com>
2015-03-05 14:59:39 +1000

    Update uq9db_handler.java

--- app/src/main/java/pt/projecttransporter/uq9db_handler.java
+++ app/src/main/java/pt/projecttransporter/uq9db_handler.java
@@ -103,4 +103,29 @@ public class uq9db_handler extends SQLiteOpenHelper{
 		// Create tables again
 		onCreate(db);
 	}
+	
+	// Is database exist on path?
+	private boolean isDatabaseExist(){
+		
+		SQLiteDatabase db = null;
+		try{
+			// Build database path
+			String dbpath = DATABASE_PATH + DATABASE_NAME;
+			db = SQLiteDatabase.openDatabase(dbpath, null, SQLiteDatabase.OPEN_READWRITE);
+			db.setLocale(Locale.getDefault());
+			db.setVersion(1);
+
+		} catch (SQLiteException e){			
+			Log.e("SQlHelper", "database not found");
+		}
+
+		// If database exist on the path, then close it and don't do anything to it
+		// and return true
+		if (db !=null){
+			db.close();
+		}
+
+		// Otherwise return false to indicate it is not there
+		return db !=null ? true:false;
+	}
 }
